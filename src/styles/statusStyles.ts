528c2587b337197f39a8ca30cb5a5c0c7c6a7b49
Nick <nick@zerjeski.de>
2025-06-07 13:24:14 +0200

    edited styling

--- src/styles/statusStyles.ts
+++ src/styles/statusStyles.ts
@@ -3,7 +3,7 @@ import { Status } from "@/types/Status";
 export const containerStyle: Record<Status, string> = {
   [Status.ON_TRACK]: 'bg-green-50 border border-green-200',
   [Status.AT_RISK]: 'bg-yellow-50 border border-yellow-200',
-  [Status.OFF_TRACK]: 'bg-orange-50 border border-red-200',
+  [Status.OFF_TRACK]: 'bg-red-50 border border-red-300',
   [Status.NOT_STARTED]: 'bg-gray-50 border border-gray-200',
   [Status.ACHIEVED]: 'bg-blue-50 border border-blue-200',
 };
@@ -11,7 +11,7 @@ export const containerStyle: Record<Status, string> = {
 export const statusLabelStyle: Record<Status, string> = {
   [Status.ON_TRACK]: 'bg-green-200 text-green-800',
   [Status.AT_RISK]: 'bg-yellow-200 text-yellow-800',
-  [Status.OFF_TRACK]: 'bg-orange-200 text-red-800',
+  [Status.OFF_TRACK]: 'bg-red-200 text-red-800',
   [Status.NOT_STARTED]: 'bg-gray-200 text-gray-800',
   [Status.ACHIEVED]: 'bg-blue-200 text-blue-800',
 };
