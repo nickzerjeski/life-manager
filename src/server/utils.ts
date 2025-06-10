09d3a7a806ee06c8cb3f7fce4d9362712bce883a
Nick Zerjeski <57059725+nickzerjeski@users.noreply.github.com>
2025-06-10 11:19:55 +0200

    move server data module

---
+++ src/server/utils.ts
@@ -0,0 +1,11 @@
+import http from 'node:http'
+
+export function parseBody(req: http.IncomingMessage): Promise<string> {
+  return new Promise(resolve => {
+    let body = ''
+    req.on('data', chunk => {
+      body += chunk
+    })
+    req.on('end', () => resolve(body))
+  })
+}
