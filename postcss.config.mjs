5b52895653fa967118988c01e353f3841c7c431a
Nick <nick@zerjeski.de>
2025-06-11 14:03:57 +0200

    tried to fix the issue

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': { config: './tailwind.config.js' },
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
