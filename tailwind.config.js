61bf135c6c4741a041bf3d57c631e670e72afa2c
Nick Zerjeski <57059725+nickzerjeski@users.noreply.github.com>
2025-06-11 13:30:27 +0200

    fix tailwind integration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-import type { Config } from "tailwindcss";
-
-export default {
-	darkMode: "class",
-    content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/styles/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
   	extend: {
@@ -91,5 +90,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate"), require("flowbite/plugin")],
-} satisfies Config;
+  plugins: [require('tailwindcss-animate'), require('flowbite/plugin')],
+};
