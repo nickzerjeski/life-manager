4a76ff9009ae4f94d8dbd7c3d5c6102120dde868
Nick <nick@zerjeski.de>
2025-06-18 19:23:23 +0200

    extended doc metadata table

--- backend/setup/doc_metadata.sql
+++ backend/setup/doc_metadata.sql
@@ -3,5 +3,8 @@ CREATE TABLE document_metadata (
     title TEXT,
     url TEXT,
     created_at TIMESTAMP DEFAULT NOW(),
-    schema TEXT
+    schema TEXT,
+    goal_id UUID REFERENCES public.goals(id),
+    project_id UUID REFERENCES public.projects(id),
+    topic_id UUID REFERENCES public.topics(id)
 );
