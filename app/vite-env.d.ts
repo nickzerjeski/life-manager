7b6c56feac7367c0ed29a8eee8ba57184c7c4915
Nick <nick@zerjeski.de>
2025-06-20 11:05:07 +0200

    added variable

--- app/vite-env.d.ts
+++ app/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_STORAGE_URL: string;
   readonly VITE_SUPABASE_ANON_KEY: string;
   readonly VITE_DOCUMENT_ENCODING_WEBHOOK?: string;
+  readonly VITE_RAG_AGENT_WEBHOOK?: string;
   // add more as needed
 }
 
